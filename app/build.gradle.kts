1903459d1729a3392c8bffaa2ddedba317a16dff
Marcia <marciacrisrs@gmail.com>
2026-08-05 22:35:44 -0300

    ajuste gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.ksp)
     alias(libs.plugins.hilt)
 }
 
