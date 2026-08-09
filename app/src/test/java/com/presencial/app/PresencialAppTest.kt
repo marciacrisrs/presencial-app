c6322491f6cc427435c8a77aabd597ab8ad57f64
Marcia <marciacrisrs@gmail.com>
2026-08-09 10:07:55 -0300

    finalizacao

--- app/src/test/java/com/presencial/app/PresencialAppTest.kt
+++ app/src/test/java/com/presencial/app/PresencialAppTest.kt
@@ -1,51 +1,34 @@
 package com.presencial.app
 
-import android.os.Looper
-import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import com.presencial.app.notification.NotificationScheduler
-import io.mockk.every
 import io.mockk.mockk
-import io.mockk.mockkStatic
-import io.mockk.unmockkStatic
 import org.junit.After
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 
-@RunWith(RobolectricTestRunner::class)
-@Config(manifest = Config.NONE, sdk = [33])
 class PresencialAppTest {
 
     @Before
     fun setup() {
-        mockkStatic(Looper::class)
-        mockkStatic(Log::class)
-        every { Looper.getMainLooper() } returns mockk(relaxed = true)
-        every { Log.w(any(), any<String>()) } returns 0
-        every { Log.w(any(), any<String>(), any()) } returns 0
-        every { Log.w(any(), any<Throwable>()) } returns 0
+        // Só mantenha os mocks de Android se forem realmente necessários
     }
 
     @After
     fun tearDown() {
-        unmockkStatic(Looper::class)
-        unmockkStatic(Log::class)
+        // Limpeza dos mocks, caso existam
     }
 
     @Test
     fun workManagerConfiguration_returns_configuration_with_worker_factory() {
         val app = PresencialApp()
-        val mockWorkerFactory = mockk<HiltWorkerFactory>()
-        val mockNotificationScheduler = mockk<NotificationScheduler>()
-        
-        app.workerFactory = mockWorkerFactory
-        app.notificationScheduler = mockNotificationScheduler
-        
+
+        app.workerFactory = mockk<HiltWorkerFactory>()
+        app.notificationScheduler = mockk<NotificationScheduler>()
+
         val config = app.workManagerConfiguration
+
         assertNotNull(config)
     }
 }
