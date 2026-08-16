80386c420d4622d0a55f8378a1e6065de41ec908
Marcia <57639202+marciacrisrs@users.noreply.github.com>
2026-08-16 18:26:49 -0300

    test(location): cover monitoring banner navigation

---
+++ app/src/androidTest/java/com/presencial/app/presentation/components/MonitoringStatusBannerTest.kt
@@ -0,0 +1,36 @@
+package com.presencial.app.presentation.components
+
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNode
+import androidx.compose.ui.test.performClick
+import com.presencial.app.ui.theme.PresencialTheme
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+
+class MonitoringStatusBannerTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun banner_click_invokesNavigationCallback() {
+        var clicked = false
+
+        composeTestRule.setContent {
+            PresencialTheme {
+                MonitoringStatusBanner(
+                    activeAddressCount = 1,
+                    foregroundGranted = false,
+                    backgroundGranted = false,
+                    onClick = { clicked = true }
+                )
+            }
+        }
+
+        composeTestRule.onNode(hasClickAction()).performClick()
+
+        assertTrue(clicked)
+    }
+}
