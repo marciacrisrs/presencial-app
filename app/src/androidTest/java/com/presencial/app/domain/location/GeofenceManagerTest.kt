ac7d27279c3dbb2cacd55fdaac40155a52f0f208
Marcia <57639202+marciacrisrs@users.noreply.github.com>
2026-08-16 19:24:44 -0300

    test: migrate geofence instrumentation test to registrar contract

--- app/src/androidTest/java/com/presencial/app/domain/location/GeofenceManagerTest.kt
+++ app/src/androidTest/java/com/presencial/app/domain/location/GeofenceManagerTest.kt
@@ -8,12 +8,14 @@ import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.GeofencingRequest
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.tasks.Task
+import com.presencial.app.data.location.AndroidGeofenceRegistrar
 import com.presencial.app.domain.model.WorkAddress
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.unmockkStatic
 import io.mockk.verify
+import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -24,13 +26,13 @@ class GeofenceManagerTest {
 
     private val context: Context = ApplicationProvider.getApplicationContext()
     private val geofencingClient: GeofencingClient = mockk(relaxed = true)
-    private lateinit var geofenceManager: GeofenceManager
+    private lateinit var geofenceRegistrar: AndroidGeofenceRegistrar
 
     @Before
     fun setup() {
         mockkStatic(LocationServices::class)
         every { LocationServices.getGeofencingClient(any<Context>()) } returns geofencingClient
-        geofenceManager = GeofenceManager(context)
+        geofenceRegistrar = AndroidGeofenceRegistrar(context)
     }
 
     @After
@@ -39,24 +41,34 @@ class GeofenceManagerTest {
     }
 
     @Test
-    fun registerGeofences_withEmptyList_callsRemoveGeofences() {
-        geofenceManager.registerGeofences(emptyList())
+    fun registerGeofences_withEmptyList_callsRemoveGeofences() = runBlocking {
+        val mockTask: Task<Void> = mockk()
+        every { mockTask.isComplete } returns true
+        every { mockTask.isSuccessful } returns true
+        every { mockTask.exception } returns null
+        every { geofencingClient.removeGeofences(any<PendingIntent>()) } returns mockTask
+
+        geofenceRegistrar.registerGeofences(emptyList())
+
         verify { geofencingClient.removeGeofences(any<PendingIntent>()) }
     }
 
     @Test
-    fun registerGeofences_withActiveAddresses_callsAddGeofences() {
+    fun registerGeofences_withActiveAddresses_callsAddGeofences() = runBlocking {
         val addresses = listOf(
             WorkAddress(
                 id = 1, name = "Home", addressText = "Street 1",
                 latitude = 1.0, longitude = 1.0, isActive = true
             )
         )
-        
-        val mockTask: Task<Void> = mockk(relaxed = true)
+
+        val mockTask: Task<Void> = mockk()
+        every { mockTask.isComplete } returns true
+        every { mockTask.isSuccessful } returns true
+        every { mockTask.exception } returns null
         every { geofencingClient.addGeofences(any(), any()) } returns mockTask
 
-        geofenceManager.registerGeofences(addresses)
+        geofenceRegistrar.registerGeofences(addresses)
 
         verify {
             geofencingClient.addGeofences(
